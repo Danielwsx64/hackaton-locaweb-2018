4093228e5dfb9519b4078dc70d5c9a0482b1fc74
Danielwsx64 <danielws.x64@gmail.com>
2018-05-26 20:19:50 -0300

    Add show, new and create to Service resource

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,4 +2,28 @@ class ServicesController < ApplicationController
   def index
     @services = Service.all
   end
+
+  def new
+    @service = Service.new
+  end
+
+  def show
+    @service = Service.find params[:id]
+  end
+
+  def create
+    @service = Service.create(service_params)
+
+    if @service
+      redirect_to service_path(@service)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def service_params
+    params.require(:service).permit(:title, :description, :duration, :price)
+  end
 end
